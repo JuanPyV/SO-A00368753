a5d703139f2ff08373b0aa1a2b0f430b5789c4a7
Juan Pablo Velazco Velasquez <A00368753@itesm.mx>
2019-10-02 00:08:46 +0000

    Practica 4

---
+++ signals/signals.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+
+int infinito = 1;
+
+void signalHandler(int signalNumber){
+    printf("Signal received %d \n", signalNumber);
+    infinito = 0;
+}
+
+int main(){
+    int pid;
+    signal(10,signalHandler);
+    pid = fork();
+    if(pid == 0){
+        printf("Soy el hijo \n");
+        while (infinito) {
+            printf("dentro del infinito\n");
+            sleep(1);
+        }
+        printf("Ya sali\n");
+        for(;;);
+    }else{
+        sleep(1);
+        printf("Soy el padre \n");
+        kill(pid, 10);
+        for(;;);
+    }
+}
