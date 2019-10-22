81cc47d3678eb989de15bbe9713ee29b30961fbc
Juan Pablo Velazco Velasquez <A00368753@itesm.mx>
2019-10-22 19:03:47 +0000

    Proyecto2

---
+++ xv6-public/sleep.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+# include "user.h"
+
+int main(int argc, char **argv){
+    int i;
+    if(argc >= 2){
+        printf(1, "Tiempo pa dormir \n");
+        i = atoi(argv[1])*100;
+        sleep(i);
+    }
+    printf(1, "Tiempo de levantarse \n");
+    exit();
+
+}
